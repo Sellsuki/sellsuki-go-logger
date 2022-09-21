6ae4cd83855dd20cfc00aa500e6c455b779aa953
SpeedxPz <admin@4th.in.th>
2022-09-21 11:14:43 +0700

    test

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Sellsuki/sellsuki-logger
+
+go 1.18
+
+require (
+	go.uber.org/zap v1.21.0
+)
---
+++ sellsuki-logger.go
@@ -0,0 +1,8 @@
+package sellsuki_logger
+
+import "fmt"
+
+func HelloWorld(name string) string {
+	message := fmt.Sprintf("Hello World, %s", name)
+	return message
+}
