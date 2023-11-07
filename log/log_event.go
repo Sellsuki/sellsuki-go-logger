28894264e6a47930bc8f6703aec11fe1a50ed105
nv4re <tosbodesz@gmail.com>
2023-11-08 01:20:20 +0700

    Remove unused code

--- log/log_event.go
+++ log/log_event.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"encoding/json"
 	"github.com/Sellsuki/sellsuki-go-logger/config"
 	"github.com/Sellsuki/sellsuki-go-logger/level"
 	"go.uber.org/zap"
@@ -9,8 +10,6 @@ import (
 type EventAction string
 type EventResult string
 
-// EventPayload represents the payload of an event log.
-//
 // EventPayload holds information about an event, including the entity being
 // acted upon, its unique reference identifier, the action being performed,
 // the result of the action, and the JSON string of event data.
@@ -19,7 +18,8 @@ type EventPayload struct {
 	ReferenceID string      `json:"reference_id"` // ReferenceID is the unique identifier of the entity, e.g., "ODR_1234567890".
 	Action      EventAction `json:"action"`       // Action represents the action being performed, such as EventActionCreate, EventActionUpdate, or EventActionDelete.
 	Result      EventResult `json:"result"`       // Result represents the result of the action, such as EventResultSuccess or EventResultFailure.
-	Data        string      `json:"data"`         // Data is a JSON string containing event data.
+	Data        any         `json:"-"`            // Data is a JSON string containing event data.
+	DataJSON    []byte      `json:"data"`         // DataJSON is a JSON string containing event data.
 }
 
 type Event struct {
@@ -38,6 +38,11 @@ const (
 func NewEvent(logger *zap.Logger, cfg config.Config, msg string, payload EventPayload) Log {
 	l := New(logger, cfg, level.Info)
 	l.SetMessage(msg)
+
+	if payload.Data != nil {
+		payload.DataJSON, _ = json.Marshal(payload.Data)
+	}
+
 	l.WithField("event", payload)
 
 	return l
